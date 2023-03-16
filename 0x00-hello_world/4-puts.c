af0678ccd36f10d32ce91e926bf8b041d3ef44b6
inyambeGoodness <goodnessjoker@gmail.com>
2023-03-16 15:36:15 -0700

    a C program that prints...

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * puts - Print the message that we enter
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
