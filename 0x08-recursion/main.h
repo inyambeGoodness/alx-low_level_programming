40d575bcb24304184ab69de29bdc5ae41b53ded7
inyambeGoodness <goodnessjoker@gmail.com>
2023-04-06 13:17:00 +0000

    main header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,7 @@
 #define FILE_MAIN
 
 int _putchar(char c);
-void void _puts_recursion(char *s);
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strnlen_recursion(char *s);
 int factorial(int n);
